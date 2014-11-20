95820120d1b47ba34566d21b5b4c1ee0bbc6c508
radman404 <radman404@users.noreply.github.com>
2014-11-20 22:00:22 +0000

    clean exit on keyboard interrupt

--- bdb.py
+++ bdb.py
@@ -1,6 +1,8 @@
 from twitch_chatters import search_all_games, remove_offline
 
 while True:
-    search_all_games()
-    remove_offline()
-
+    try:
+        search_all_games()
+        remove_offline()
+    except KeyboardInterrupt:
+        exit()
